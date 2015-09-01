079c067366c0985867573215a85e9661495b7994
Tanin Rojanapiansatith <sp_land@hotmail.com>
2015-09-01 16:04:42 +0700

    connect widget of view and code side(ctrl + view)

--- PracPlayDice/ViewController.swift
+++ PracPlayDice/ViewController.swift
@@ -10,6 +10,19 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var diceImageView: UIImageView!
+    
+    @IBAction func redButton(sender: UIButton) {
+    }
+    
+    @IBAction func greenButton(sender: UIButton) {
+    }
+    
+    
+    @IBAction func blueButton(sender: UIButton) {
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
